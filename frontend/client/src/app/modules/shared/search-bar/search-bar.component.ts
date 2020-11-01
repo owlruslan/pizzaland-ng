78f511c8540bffea0378c9792722bb1557aea347
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-11-01 18:49:53 +0500

    Add Reactive form, add searchQuery field to this form

--- frontend/client/src/app/modules/shared/search-bar/search-bar.component.ts
+++ frontend/client/src/app/modules/shared/search-bar/search-bar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  public searchForm: FormGroup
 
-  ngOnInit(): void {
+  private get searchQuery(): FormControl {
+    return this.searchForm.controls.searchQuery as FormControl;
+  }
+
+  constructor (private formBuilder: FormBuilder) { }
+
+  ngOnInit (): void {
+    this.searchForm = this.formBuilder.group({
+      searchQuery: [''],
+    })
   }
 
 }
--- frontend/client/src/app/modules/shared/search-bar/search-bar.module.ts
+++ frontend/client/src/app/modules/shared/search-bar/search-bar.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './search-bar.component';
+import { ReactiveFormsModule } from '@angular/forms'
 
 
 
 @NgModule({
   declarations: [SearchBarComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ReactiveFormsModule,
+  ],
 })
 export class SearchBarModule { }
