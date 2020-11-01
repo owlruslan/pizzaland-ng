495fd14c3afae435345001675da3ec13097f3766
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-11-01 17:57:15 +0500

    Generate search-bar module and component via CLI

---
+++ frontend/client/src/app/modules/shared/search-bar/search-bar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchBarComponent } from './search-bar.component';
+
+
+
+@NgModule({
+  declarations: [SearchBarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SearchBarModule { }
