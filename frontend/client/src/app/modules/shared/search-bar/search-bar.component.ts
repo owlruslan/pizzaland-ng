53beeb5933a9dc08d7d3fd0756b8332156e851fc
Ruslan Usmanov <ruslanusm6@gmail.com>
2020-11-01 22:02:55 +0500

    Add searching process in search-bar

--- frontend/client/src/app/modules/shared/search-bar/search-bar.component.ts
+++ frontend/client/src/app/modules/shared/search-bar/search-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core'
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
 import { SearchService } from '@app/services/search/search.service'
+import { BehaviorSubject } from 'rxjs'
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators'
 
 @Component({
   selector: 'app-search-bar',
@@ -11,26 +13,45 @@ export class SearchBarComponent implements OnInit {
 
   public searchForm: FormGroup
   public isViewActive = false
+  public isSearching = false
 
-  private get searchQuery (): FormControl {
+  public searchResults$ = new BehaviorSubject([])
+
+  private get searchQuery(): FormControl {
     return this.searchForm.controls.searchQuery as FormControl
   }
 
   @HostListener('document:click', ['$event'])
-  clicked (event) {
+  clicked(event) {
     if (!this.elementRef.nativeElement.contains(event.target)) {
       this.isViewActive = false
     }
   }
 
-  constructor (
+  constructor(
     private formBuilder: FormBuilder, private elementRef: ElementRef,
     private searchService: SearchService) { }
 
-  ngOnInit (): void {
-    this.searchForm = this.formBuilder.group({
-      searchQuery: [''],
+  public search(query = ''): void {
+    this.searchResults$.next([])
+    this.isSearching = true
+
+    this.searchService.getSearchResults(query).toPromise().then(result => {
+      this.isSearching = false
+      this.searchResults$.next(result.data)
     })
   }
 
+  ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      searchQuery: [''],
+    })
+
+    this.searchQuery.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+    )
+      .subscribe(
+        (query: string) => this.search(query))
+  }
 }
