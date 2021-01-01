e12890524a36efc4ba2fa06e390298d871c1a6ff
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-01-01 09:29:52 +0500

    Remove search

--- projects/restaurants/src/app/modules/restaurants/restaurants.module.ts
+++ projects/restaurants/src/app/modules/restaurants/restaurants.module.ts
@@ -1,27 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { RestaurantsRoutingModule } from './restaurants-routing.module';
-import { RestaurantsComponent } from './restaurants.component';
-import { NavbarModule } from '../shared/navbar/navbar.module';
-import { SearchService } from '../../services/search/search.service';
-import { SearchBarModule } from '../shared/search-bar/search-bar.module';
-import { RestaurantCardModule } from '../shared/restaurant-card/restaurant-card.module';
-import { SearchMockService } from '../../services/search/search-mock.service';
-import { RestaurantsMockService } from '../../services/restaurants/restaurants-mock.service';
-import { RestaurantsService } from '../../services/restaurants/restaurants.service';
+import {RestaurantsRoutingModule} from './restaurants-routing.module';
+import {RestaurantsComponent} from './restaurants.component';
+import {NavbarModule} from '../shared/navbar/navbar.module';
+import {RestaurantCardModule} from '../shared/restaurant-card/restaurant-card.module';
+import {RestaurantsMockService} from '../../services/restaurants/restaurants-mock.service';
+import {RestaurantsService} from '../../services/restaurants/restaurants.service';
 
 @NgModule({
   declarations: [RestaurantsComponent],
   imports: [
     CommonModule,
     RestaurantsRoutingModule,
-    SearchBarModule,
     NavbarModule,
     RestaurantCardModule,
   ],
   providers: [
-    { provide: SearchService, useClass: SearchMockService },
     { provide: RestaurantsService, useClass: RestaurantsMockService },
   ],
 })
--- projects/restaurants/src/app/services/search/search-api.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from 'rxjs';
-import { Restaurant } from '../../models/restaurants/restaurant';
-
-export interface SearchAPI {
-
-  getSearchResults(query: string): Observable<Restaurant[]>;
-}
