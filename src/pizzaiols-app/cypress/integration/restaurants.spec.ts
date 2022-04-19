86ee33917f5b2c5cb61a6c7a0004855dab46e24a
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-04-20 02:39:34 +0500

    add restaurants simple test

--- src/pizzaiols-app/cypress/integration/restaurants.spec.ts
+++ src/pizzaiols-app/cypress/integration/restaurants.spec.ts
@@ -1,7 +1,6 @@
 describe('Restaurants', () => {
   it('Shows list of restaurants as a home screen of app', () => {
     cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
+    cy.contains('Restaurants')
   })
 })
