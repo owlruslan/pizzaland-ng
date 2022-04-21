dd368d355360fd2d70da4a6479e4a14b9e89c10e
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-04-21 07:25:34 +0500

    add create restaurant method

--- src/restaurants-service/app/controllers/api/v1/restaurants_controller.rb
+++ src/restaurants-service/app/controllers/api/v1/restaurants_controller.rb
@@ -1,9 +1,23 @@
 # TODO: https://medium.com/swlh/how-to-build-an-api-with-ruby-on-rails-28e27d47455a
 class Api::V1::RestaurantsController < ApplicationController
-  # Renders all items in the products table
+  # Renders all items in the restaurants table
   def index
-    products = Products.all
-    render json: products, status: 200
+    restaurants = Restaurants.all
+    render json: restaurants, status: 200
+  end
+
+  # Creates new restaurant.
+  def create
+    restaurant = Restaurant.new(
+      name: prod_params[:name],
+      rating: prod_params[:rating],
+      address: prod_params[:address],
+    )
+    if restaurant.save
+      render json: restaurant, status: 200
+    else
+      render json: {error: 'Error creating.'}
+    end
   end
 
   def show
