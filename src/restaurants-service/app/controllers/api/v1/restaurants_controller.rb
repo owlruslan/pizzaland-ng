091fd3f65715cf101316408aff2e665896e31635
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-04-21 07:19:43 +0500

    add all restaurants method

--- src/restaurants-service/app/controllers/api/v1/restaurants_controller.rb
+++ src/restaurants-service/app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,9 @@
+# TODO: https://medium.com/swlh/how-to-build-an-api-with-ruby-on-rails-28e27d47455a
 class Api::V1::RestaurantsController < ApplicationController
+  # Renders all items in the products table
   def index
+    products = Products.all
+    render json: products, status: 200
   end
 
   def show
