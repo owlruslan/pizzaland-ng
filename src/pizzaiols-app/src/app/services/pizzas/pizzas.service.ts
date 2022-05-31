09353fb6949dcc14c48b462c60fe3456b4106e54
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-05-31 23:04:44 +0500

    optimize imports

--- src/pizzaiols-app/src/app/services/pizzas/pizzas.service.ts
+++ src/pizzaiols-app/src/app/services/pizzas/pizzas.service.ts
@@ -1,4 +1,3 @@
-import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {environment} from '../../../environments/environment';
