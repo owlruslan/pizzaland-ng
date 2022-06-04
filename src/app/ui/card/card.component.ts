ec57648af8bf684d735a2c7293c272eff94e7324
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-06-04 18:29:51 +0500

    remove unused component

--- src/app/ui/card/card.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'ui-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss'],
-})
-export class CardComponent {
-}
